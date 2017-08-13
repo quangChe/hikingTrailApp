ee64a9c55e2d3d3a51fc122ce89209424c1db127
Quang Che <quang.n.che@gmail.com>
2017-08-12 23:27:04 -0700

    Stylistic changes, refactored code to separate data and styles from app.js (VM controller) and did some revision

---
+++ scripts/data.js
@@ -0,0 +1,83 @@
+// All the hikes' information
+var hikes = [
+    {
+        name: "Point Dume Cove",
+        location: {lat: 34.00190531306304, lng: -118.8067102432251},
+        distance: "1.2 miles",
+        difficulty: "Easy",
+        photo: "https://www.cliffsidemalibu.com/wp-content/uploads/2016/01/Malibu-beach.jpg",
+        id: 1
+    },
+    {
+        name: "Escondido Falls",
+        location: {lat: 34.02889203871035, lng: -118.77802189499091},
+        distance: "3.7 miles",
+        difficulty: "Easy",
+        photo: "https://www.hikespeak.com/img/la/Escondido/Escondido_Falls_2616.jpg",
+        id: 2
+    },
+    {
+        name: "Solstice Canyon",
+        location: {lat: 34.03382125031014, lng: -118.74168472786972},
+        distance: "3.4 miles",
+        difficulty: "Moderate",
+        photo: "https://www.hikespeak.com/img/la/solstice/Solstice_Canyon_Trail_IMG_2880.jpg",
+        id: 3
+    },
+    {
+        name: "Los Liones Trail",
+        location: {lat: 34.04665214684968, lng: -118.5588722646525},
+        distance: "10.4 miles",
+        difficulty: "Hard",
+        photo: "https://kkanemor.files.wordpress.com/2013/11/20131127-210036.jpg",
+        id: 4
+    },
+    {
+        name: "Santa Ynez Trailhead",
+        location: {lat: 34.07715965358875, lng: -118.56576329871294},
+        distance: "2.2 miles",
+        difficulty: "Easy",
+        photo: "https://www.hikespeak.com/img/la/Topanga/Santa_Ynez_Falls_IMG_8635.jpg",
+        id: 5
+    },
+    {
+        name: "Temescal Canyon",
+        location: {lat: 34.09324617541828, lng: -118.5863935947418},
+        distance: "3 miles",
+        difficulty: "Easy",
+        photo: "http://www.photographyontherun.com/content/binary/PoisonOakSantaYnezTr_3400b.jpg",
+        id: 6
+    },
+    {
+        name: "Eaton Canyon",
+        location: {lat: 34.17771036218618, lng: -118.09823416957826},
+        distance: "3.6 miles",
+        difficulty: "Moderate",
+        photo: "http://3.bp.blogspot.com/-tccgIFzQUIc/UUUA8u-8gkI/AAAAAAAAH5w/SzWJ6wXubF4/s1600/water1.jpg",
+        id: 7
+    },
+    {
+        name: "Sturtevant Falls",
+        location: {lat: 34.16779908705499, lng: -118.03180257528363},
+        distance: "3.1 miles",
+        difficulty: "Moderate",
+        photo: "http://static.panoramio.com/photos/large/61261459.jpg",
+        id: 8
+    },
+    {
+        name: "Bridge to Nowhere",
+        location: {lat: 34.283226, lng: -117.746991},
+        distance: "11.3 miles",
+        difficulty: "Hard",
+        photo: "https://media-cdn.tripadvisor.com/media/photo-s/02/5d/7b/14/the-bridge-to-nowhere.jpg",
+        id: 9
+    },
+    {
+        name: "Crystal Cove",
+        location: {lat: 33.56641084249367, lng: -117.82154058827237},
+        distance: "4 miles",
+        difficulty: "Moderate",
+        photo: "http://www.visitnewportbeach.com/wp-content/uploads/2013/06/cc-700x400.jpg",
+        id: 10
+    }
+];
--- scripts/styles.js
+++ scripts/styles.js
@@ -1,12 +1,23 @@
 // Effects for navbar
-
 function openNav() {
     $('#navBtn').toggle();
     $('#options').toggle('slide');
-
 }
 
 function closeNav() {
     $('#navBtn').toggle('slow');
     $('#options').toggle('slide');
+}
+
+// Show difficulty colors
+function difficultyColorRender(element, difficultyValue) {
+    if (difficultyValue == "Easy") {
+        $(element).css("color", "#6bbd71");
+    }
+    if (difficultyValue == "Moderate") {
+        $(element).css("color", "#e2b265");
+    }
+    if (difficultyValue == "Hard") {
+        $(element).css("color", "#e86e6e");
+    }
 }
